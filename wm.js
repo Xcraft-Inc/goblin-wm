c3d80507f278c8433ee2730099a77e7b4bae0de2
Samuel Loup <loup@epsitec.ch>
2016-08-10 14:52:36 +0200

    Add first impl of goblin-wm

---
+++ wm.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const path    = require ('path');
+const service = require ('./lib/service.js');
+
+/**
+ * Retrieve the list of available commands.
+ *
+ * @returns {Object} The list and definitions of commands.
+ */
+exports.xcraftCommands = function () {
+  const xUtils = require ('xcraft-core-utils');
+  return {
+    handlers: service,
+    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json'))
+  };
+};
