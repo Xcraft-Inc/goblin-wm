95712db406c3545e2ed5801d45b446d563ba967a
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-13 17:12:10 +0200

    Use the new Xcraft module declaration

--- wm.js
+++ wm.js
@@ -1,17 +1,37 @@
 'use strict';
 
-const path = require ('path');
-const service = require ('./lib/service.js');
-
 /**
  * Retrieve the list of available commands.
  *
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function () {
-  const xUtils = require ('xcraft-core-utils');
   return {
-    handlers: service,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
+    handlers: require ('./lib/service.js'),
+    rc: {
+      'win.create': {
+        parallel: true,
+        desc: 'Open a new window',
+        options: {
+          params: {},
+        },
+      },
+      'win.delete': {
+        parallel: true,
+        desc: 'Delete window',
+        options: {
+          params: {
+            required: 'wid',
+          },
+        },
+      },
+      'win.list': {
+        parallel: true,
+        desc: 'List managed windows',
+        options: {
+          params: {},
+        },
+      },
+    },
   };
 };
