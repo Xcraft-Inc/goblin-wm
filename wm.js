ccb861e700d1268ca98c6cebdc61b0ec3da75f27
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-02-21 00:09:53 +0100

    Update xcraft-dev-rules to v0.2 and prettify everything

--- wm.js
+++ wm.js
@@ -5,6 +5,6 @@
  *
  * @returns {Object} The list and definitions of commands.
  */
-exports.xcraftCommands = function () {
-  return require ('./lib/service.js');
+exports.xcraftCommands = function() {
+  return require('./lib/service.js');
 };
