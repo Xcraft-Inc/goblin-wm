9aa1bf1fd0202f00bde20bb0d2273fce7bbc0da9
Yannick Vessaz <vessaz@epsitec.ch>
2019-04-12 12:18:46 +0200

    Improve BrowserWindow initial size
    The window size is relative to the work area

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Retrieve the inquirer definition for xcraft-core-etc
+ */
+module.exports = [
+  {
+    type: 'input',
+    name: 'windowOptions',
+    message: 'Options for electron BrowserWindow',
+    default: null,
+  },
+];
