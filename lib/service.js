Xcraft-Inc/goblin-wm

commit c97a526a6a44862df76751450b3e27240869570e

workaround: disable jitter

lib/service.js
@@ -37,6 +37,8 @@ Goblin.registerQuest(goblinName, 'create', function* (
   cookies,
   next
 ) {
+  //FIXME: disable jitter
+  quest.goblin.setX('jitterOn', false);
   quest.do();
   //Manage by feed subs
   quest.goblin.setX('feedSubs', {});
@@ -156,11 +158,14 @@ Goblin.registerQuest(goblinName, 'create', function* (
       return;
     }
 
-    channel.sendAction(
-      payload.noJitter
-        ? {type: 'NO_JITTER', ...payload}
-        : {type: 'PUSH_JITTER', ...payload}
-    );
+    const jitterOn = quest.goblin.getX('jitterOn');
+    if (jitterOn) {
+      channel.sendAction(
+        payload.noJitter
+          ? {type: 'NO_JITTER', ...payload}
+          : {type: 'PUSH_JITTER', ...payload}
+      );
+    }
 
     if (payload.overlay) {
       onReconnect('attempt');

commit dd9118b8ffef1fa74c3f0a35ff268c4aefb78d09

Fix regression introduced by c97a526a6a44862df76751450b3e27240869570e
It shoud prevent the overlay to persist (false positive).

lib/service.js
@@ -144,9 +144,11 @@ Goblin.registerQuest(goblinName, 'create', function* (
         action.disconnected = true;
         action.message =
           'The client has lost the connection with the server and attempts to reconnect...';
+        quest.goblin.setX('isDisconnected', true);
       } else {
         action.disconnected = false;
         action.message = '...';
+        quest.goblin.setX('isDisconnected', false);
       }
       channel.sendAction(action);
     }
@@ -158,13 +160,29 @@ Goblin.registerQuest(goblinName, 'create', function* (
       return;
     }
 
+    /* FIXME: this jitterOn must be removed when we are able to use multi-stores
+     * in the frontend in order to seperate the redux connect calls used only
+     * for the infrastructure.
+     */
     const jitterOn = quest.goblin.getX('jitterOn');
+    const isDisconnected = quest.goblin.getX('isDisconnected');
     if (jitterOn) {
       channel.sendAction(
         payload.noJitter
           ? {type: 'NO_JITTER', ...payload}
           : {type: 'PUSH_JITTER', ...payload}
       );
+    } else if (isDisconnected && !payload.noJitter) {
+      /* Because the jitter is disabled in the frontend, we must remove the
+       * overlay when appropriate. When jitterOn is false, this code is
+       * useless because PUSH_JITTER reset the disconnected flag.
+       */
+      channel.sendAction({
+        type: 'SET_DISCONNECTED',
+        disconnected: false,
+        message: '...',
+      });
+      quest.goblin.setX('isDisconnected', false);
     }
 
     if (payload.overlay) {